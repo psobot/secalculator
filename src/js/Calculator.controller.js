b6fdd4fb2ca763be50bd971a121bd957fd3d99f7
Peter Sobot <psobot@gmail.com>
2014-09-08 20:20:52 -0400

    Added load/save in URL bar. Fixes #2.

--- src/js/Calculator.controller.js
+++ src/js/Calculator.controller.js
@@ -13,4 +13,42 @@ calculator.controller('CalculatorController', function ($scope) {
       field.value = Math.round(value * 100) / 100;
     });
   };
+
+  $scope.save = function() {
+    var values = _.map($scope.data.years, function(terms) {
+      return _.map(terms, function(term) {
+        return _.map(term.fields, function(field) {
+          return parseFloat(field.value);
+        });
+      });
+    });
+    return JSON.stringify(values);
+  };
+
+  $scope.load = function(json) {
+    var local = _.clone($scope.data);
+    try {
+      var values = JSON.parse(json);
+
+      _.forEach(local.years, function(terms, i) {
+        _.forEach(terms, function(term, j) {
+          _.forEach(term.fields, function(field, k) {
+            field.value = parseFloat(values[i][j][k]);
+          });
+        });
+      });
+
+      $scope.data = local;
+    } catch (e) {
+      console.log("Could not load data: ", e);
+    }
+  };
+
+  if (window.location.hash) {
+    $scope.load(window.location.hash.slice(1));
+  }
+
+  $scope.$watch('data', function(newValue) {
+    window.location.hash = $scope.save();
+  }, true);
 });
